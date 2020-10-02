e7f712ba955512730487d0e0b807b1b4fe18183b
Thomas Stanger <thomasstanger0@gmail.com>
2020-10-02 11:45:50 +0100

    comment added

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_Computer_Science
+{
+    public class Question2
+    {
+        public bool isInBoth(List<int> list1, List<int> list2)
+        {
+            var isTheSame = true;
+            while (isTheSame)
+            {
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mylist = new List<int>(){4,2,6,7,4,13,12,5,3,1,0, -1, -4, -7, -9};
+            var mylist2 = new List<int>(){4,7,2,3,12,6};
+            var question1 = new Question1();
+            Console.WriteLine(question1.Sum(mylist));
+            Console.WriteLine(question1.Max(mylist));
+            Console.WriteLine(question1.Min(mylist));
+            question1.Negatives(mylist);
+            
+        }
+    }
+}
---
+++ Question1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_Computer_Science
+{
+    public class Question1
+    {
+        public int Sum(List<int> list)
+        {
+            var sum = 0;
+            foreach (var i in list)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+
+        public int Max(List<int> list)
+        {
+            var max = 0;
+            foreach (var i in list)
+            {
+                if (i > max)
+                    max = i;
+            }
+            return max;
+        }
+
+        public int Min(List<int> list)
+        {
+            var min = 0;
+            foreach (var i in list)
+            {
+                if (i < min)
+                    min = i;
+            }
+            return min;
+        }
+
+        public void Negatives(List<int> list)
+        {
+            var negatives = new List<int>();
+
+            foreach (var i in list)
+            {
+                if (i < 0)
+                    negatives.Add(i);
+            }
+
+            foreach (var j in negatives)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
